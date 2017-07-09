bd0842ddeb3c66b64f16a17640382ebed8066a74
Matthew Story <matthewstory@Matthews-MacBook.local>
2017-07-08 21:18:42 -0400

    IG footer working
    Signed-off-by: Matthew Story <matthewstory@Matthews-MacBook.local>

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,7 @@
 // Set up the header color switching
 $(function() {
     var colors = [ "pink", "orange", "blue", "navy" ];
-    var topClass, bottomClass;
+    var topClass, bottomClass, footerClass;
     function getRad() {
         if (topClass !== undefined) {
             $('.site-header').removeClass(topClass);
@@ -9,10 +9,16 @@ $(function() {
         if (bottomClass !== undefined) {
             $('.site-header').removeClass(bottomClass);
         }
+        if (footerClass !== undefined) {
+            $('.site-footer').removeClass(footerClass);
+        }
         topClass = colors[Math.floor(Math.random()*colors.length)] + "-top";
         bottomClass = colors[Math.floor(Math.random()*colors.length)] + "-bottom";
+        footerClass = colors[Math.floor(Math.random()*colors.length)] + "-top";
+
         $(".site-header").addClass(topClass);
         $(".site-header").addClass(bottomClass);
+        $(".site-footer").addClass(footerClass);
     }
 
     getRad();
@@ -22,13 +28,15 @@ $(function() {
 // set up insta feed
 $(function() {
     var clientId = $("#instafeed").data("instagram_clientid"),
-        userId = $("#instafeed").data("instagram_userid");
-    if (clientId !== undefined && userId !== undefined) {
+        userId = $("#instafeed").data("instagram_userid"),
+        accessToken = $("#instafeed").data("instagram_accesstoken");
+    if (clientId !== undefined && userId !== undefined && accessToken !== undefined) {
         new Instafeed({
             "get": "user",
             "userId": userId,
             "limit": 4,
             "clientId": clientId,
+            "accessToken": accessToken,
             "template": "<li class='ig-item'><a href='{{link}}'><img src='{{image}}' /></a></li>"
         }).run();
     }
